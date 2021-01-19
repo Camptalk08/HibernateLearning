220265af40d55f9d7a961f1690b0fcbde9c6e542
Camptalk08 <thangavelaj@hcl.com>
2021-01-19 11:56:13 +0530

    project Structure created

---
+++ src/main/java/com/hibernate/examples/entity/Employee.java
@@ -0,0 +1,19 @@
+package com.hibernate.examples.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Employee {
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Id
+	private int employeeId;
+	private String name;
+}
